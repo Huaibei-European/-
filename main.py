3e31cf1d1f3168cc6e700d17c14e6282b8f81c8a
Huaibei-European <68190236+Huaibei-European@users.noreply.github.com>
2021-03-31 09:49:57 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,141 @@
+import requests
+import parsel
+import re
+import csv
+
+with open('data.csv', mode='w', encoding='utf-8', newline='') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(
+        ['招生单位', '所在地', '研究生院', '自划线院校', '博士点',
+         '考试方式', '院系所', '专业', '研究方向', '学习方式', '指导教师', '拟招生人数', '跨专业', '备注',
+         '政治', '考试大纲', '英语', '考试大纲', '数学', '考试大纲', '专业课', '考试大纲',
+         '政治', '考试大纲', '英语', '考试大纲', '数学', '考试大纲', '专业课', '考试大纲', ])
+
+url = 'https://yz.chsi.com.cn/zsml/queryAction.do'
+
+data = {
+    'ssdm': '11',
+    'dwmc': '',
+    'mldm': '08',
+    'mlmc': '',
+    'yjxkdm': '0835',
+    'zymc': '',
+    'xxfs': '',
+}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
+}
+
+response = requests.post(url=url, data=data, headers=headers)
+html_data = response.text
+selector = parsel.Selector(html_data)
+
+trs = selector.css('.ch-table tbody tr')
+
+for tr in trs:
+    temp_writer = []
+    temp_all = []
+    school = tr.css('a::text').get()
+    school_url = 'https://yz.chsi.com.cn/' + tr.css('a::attr(href)').get()
+    city = tr.css('td:nth-child(2)::text').get()
+    statue_1 = tr.css('td:nth-child(3) i::text').get()
+    if not statue_1:
+        statue_1 = ''
+    else:
+        statue_1 = '是'
+    statue_2 = tr.css('td:nth-child(4) i::text').get()
+    if not statue_2:
+        statue_2 = ''
+    else:
+        statue_2 = '是'
+    statue_3 = tr.css('td:nth-child(5) i::text').get()
+    if not statue_3:
+        statue_3 = ''
+    else:
+        statue_3 = '是'
+    print('学校信息：')
+    print(school, city, statue_1, statue_2, statue_3)
+    # temp_all.append([school, city, statue_1, statue_2, statue_3])
+
+    with open('data.csv', mode='a', encoding='utf-8', newline='') as file:
+        # csv_writer = csv.writer(file)
+        # csv_writer.writerow(temp_writer)
+        file.write(school + ',' + city + ',' + statue_1 + ',' + statue_2 + ',' + statue_3 + ',')
+
+    temp_subject_Information = []
+
+    response = requests.get(url=school_url, headers=headers)
+    selector = parsel.Selector(response.text)
+    trs = selector.css('.ch-table tbody tr')
+
+    extent = selector.css('.ch-table tbody tr td:nth-child(1)::text').getall()
+    # if len(extent) >= 2:
+    #     """如果当前学科研究方向有多个，则分行写入，control_number改为1"""
+    #     control_number = 1
+    # else:
+    #     control_number = 0
+
+    Number_of_studies = len(extent)
+    control_number = 1
+
+    for tr in trs:
+        form = tr.css('td:nth-child(1)::text').get()
+        faculty = tr.css('td:nth-child(2)::text').get()
+        speciality = tr.css('td:nth-child(3)::text').get()
+        Research_Direction = tr.css('td:nth-child(4)::text').get()
+        Styles = tr.css('td:nth-child(5)::text').get()
+        teacher = tr.css('td:nth-child(6)::text').get().strip()
+        people = tr.css('td:nth-child(7) script::text').get()
+        transdisciplinary = tr.css('td:nth-child(9) a::text').get()
+        remark = tr.css('td:nth-child(10) script::text').get()
+        try:
+            people = re.findall("专业：.*?',", people, re.S)[0]
+        except:
+            people = re.findall("研究方向：.*?,", people, re.S)[0]
+        remark = eval(re.findall("cutString(.*?),", remark, re.S)[0].replace('(', ''))
+        people: str
+        people = people.split(',')[0].replace("'", '')
+
+        syllabus_url = 'https://yz.chsi.com.cn/' + tr.css('td:nth-child(8) a::attr(href)').get()
+        print('招生信息：')
+        print(form, faculty, speciality, Research_Direction, Styles, teacher, people, transdisciplinary, remark)
+
+        temp_subject_Information.append(
+            [form, faculty, speciality, Research_Direction, Styles, teacher, people, transdisciplinary, remark])
+
+        response = requests.get(url=syllabus_url, headers=headers)
+        selector = parsel.Selector(response.text)
+        tds = selector.css('tbody.zsml-res-items td')
+        print('考试科目：')
+        for td in tds:
+            politics = td.css('::text').get().strip()
+            detail = td.css('span::text').get()
+            print(politics, detail)
+
+            temp_subject_Information.append([politics, detail])
+
+        for i in temp_subject_Information:
+            for k in i:
+                temp_writer.append(k)
+
+        temp_subject_Information.clear()
+
+        with open('data.csv', mode='a', encoding='utf-8', newline='') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(temp_writer)
+
+        temp_writer.clear()
+
+        # 决定是否写入新行
+        if Number_of_studies == 1:
+            pass
+        else:
+            if control_number == Number_of_studies:
+                pass
+            else:
+                with open('data.csv', mode='a', encoding='utf-8', newline='') as file:
+                    file.write('' + ',' + '' + ',' + '' + ',' + '' + ',' + '' + ',')
+                control_number += 1
+
+    print('*' * 100)
